a976872cb6eb04e47518cbde266ab4439c06b2a0
IvanPavlovets <ivan150287@gmail.com>
2023-04-13 19:44:33 +0700

    2. Сканирование проекта [#1070]

--- src/main/java/ru/job4j/SpringDI.java
+++ src/main/java/ru/job4j/SpringDI.java
@@ -5,9 +5,7 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 public class SpringDI {
     public static void main(String[] args) {
         AnnotationConfigApplicationContext contex = new AnnotationConfigApplicationContext();
-        contex.register(Store.class);
-        contex.register(ConsoleInput.class);
-        contex.register(StartUI.class);
+        contex.scan("ru.job4j");
         contex.refresh();
         StartUI ui = contex.getBean(StartUI.class);
         ui.add("Petr Arsentev");
