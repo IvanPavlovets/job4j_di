b85a639a02881961d466374478884f01be9fec29
IvanPavlovets <ivan150287@gmail.com>
2023-04-17 12:31:52 +0700

    4. Время жизни (Scope) [#261277]

--- src/main/java/ru/job4j/StartUI.java
+++ src/main/java/ru/job4j/StartUI.java
@@ -1,14 +1,18 @@
 package ru.job4j;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 @Component
+@Scope("prototype")
 public class StartUI {
-    @Autowired
-    private ConsoleInput input;
-    @Autowired
-    private Store store;
+    private final ConsoleInput input;
+    private final Store store;
+
+    public StartUI(ConsoleInput input, Store store) {
+        this.input = input;
+        this.store = store;
+    }
 
 
     public void add(String value) {
